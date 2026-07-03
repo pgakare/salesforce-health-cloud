d515a5e0821de422831c87d2052259081d4f7515
extern-bipin-singh <extern.saurabh.taley@vwfs.com>
2026-07-03 14:46:51 +0530

    Updated deployment file

--- SalesforceHealthCloud/HC-Deployment.ps1
+++ SalesforceHealthCloud/HC-Deployment.ps1
@@ -16,5 +16,5 @@ $confirm = Read-Host "Deploy? (yes/no)"
 if ($confirm -eq "yes") {
     Write-Host " Deploying..." -ForegroundColor Green
     sf project deploy start --target-org $TargetOrg -x manifest/package.xml --wait 60
-    Write-Host "Done!" -ForegroundColor Green
+    Write-Host "Done!.." -ForegroundColor Green
 }
